c87ed3d7766a7d04de58a60b8b1910386a90d11c
Nwosa <oberetochi@gmail.com>
2022-12-08 11:36:35 -0800

    This program prints the size of computer using the sizeof() function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 	{
-		printf("size of char: %i byte(s)\n", sizeof(char));
-		printf("size of an int: %i byte(s)\n", sizeof(int));
-		printf("size of a long int: %i byte(s)\n", sizeof(long int));
-		printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
-		printf("size of float: %i byte(s)\n", sizeof(float));
+		printf("size of char: %i byte(s)\n", sizeof(char);
+		printf("size of an int: %i byte(s)\n", sizeof(int);
+		printf("size of a long int: %i byte(s)\n", sizeof(long int);
+		printf("size of a long long int: %i byte(s)\n", sizeof(long long int);
+		printf("size of float: %i byte(s)\n", sizeof(float);
 		return (0);
 	}
