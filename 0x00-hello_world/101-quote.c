7fec41ec30241d4c9ecbc971701cefe0fb82f722
Nwosa <oberetochi@gmail.com>
2022-12-08 13:31:55 -0800

    A code that prints without printf() and puts() functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: A program that returns 1
  * Return: 1
+ *
  */
 int main(void)
-	{
-		fputc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-		return (1);
-	}
+{
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ch, sizeof(ch));
+	return (1);
+}
