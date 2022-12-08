bc27e97da718aa5cab649f0e8b625b45565c1fa0
Nwosa <oberetochi@gmail.com>
2022-12-08 12:04:36 -0800

    A code that prints without printf() and puts() functions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - Entry point
+ *
+ * description: A program that returns 1
+ *
+ * retunrs: 1
+ */
+int main(void)
+	{
+		char ch[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+		write(2, ch, sizeof(ch));
+		return (1);
+	}
