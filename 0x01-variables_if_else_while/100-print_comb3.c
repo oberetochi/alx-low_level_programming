d1f7dcb100ae4f401c1a6d493d634585fae66e9e
Nwosa <oberetochi@gmail.com>
2022-12-09 15:00:39 -0800

    Prints combination of numbers 00 to 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: Prints numbers in double 00 - 99
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+		}
+		if (num1 == 9 && num2 == 9)
+		{
+			continue;
+				putchar(' ');
+			putchar(',');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
