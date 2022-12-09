907b24350ca24fcceb0e069018e748b5c69c9eb4
Nwosa <oberetochi@gmail.com>
2022-12-09 08:16:31 -0800

    Equating last digit of random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 		{
 			printf("Last digit of %i is %i and is greater than 5\n", n, last);
 		}
-		else
+		else if (last == 0)
 		{
 			printf("Last digit of %i is %i and is 0\n", n, last);
 		}
