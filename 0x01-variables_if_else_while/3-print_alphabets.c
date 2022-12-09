2e39a1bbd4e028add8fa5355d4bd877e881acc16
Nwosa <oberetochi@gmail.com>
2022-12-09 09:38:18 -0800

    Prints ALPHABETICALLY

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: Prints ALPHABETICALLY
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	ch = 'A';
+
+	while (ch <= 'Z')
+	{	
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
