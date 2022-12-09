771c1c5ec263b9d456c7cf4ddfb481712e247aac
Nwosa <oberetochi@gmail.com>
2022-12-09 15:17:49 -0800

    Prints combination of numbers 00 to 99

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,12 +16,12 @@ int main(void)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
-		}
+		
 		if (num1 == 8 && num2 == 9)
-		{
+		
 			continue;
-				putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
