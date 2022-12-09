6c0e0eb0b8bc48ff17683804ea24e54318d060b3
Nwosa <oberetochi@gmail.com>
2022-12-09 15:45:26 -0800

    Print least combinations of three numbers 000 to 999

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a program that prints all possible different
+ * combinations of three digits 000 - 999
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+					if (num1 == 8 && num2 == 9 && num3 == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
