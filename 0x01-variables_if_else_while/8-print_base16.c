46f9d064c27475629577ea8e5f0d1cab9d3f040e
Nwosa <oberetochi@gmail.com>
2022-12-09 13:26:23 -0800

    Base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: Base 16 in lowercase
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	char letter;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (letter = 'a'; letter < 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
