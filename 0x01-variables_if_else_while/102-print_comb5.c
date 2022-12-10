5588962dd69189bfe8267afefa3fb369b58050aa
Nwosa <oberetochi@gmail.com>
2022-12-09 16:14:42 -0800

    Print least combinations of three numbers 000 to 999

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description:  a program that prints all possible combinations
+ * of two two-digit numbers
+ *
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	int numba1, numba2;
+
+	for (numba1 = 0; numba1 < 98; numba1++)
+	{
+		for (numba2 = numba1 + 1; numba2 < 99; numba2++)
+		{
+			putchar((numba1 % 10) + '0');
+			putchar((numba2 % 10) + '0');
+			putchar(' ');
+			putchar((numba1 % 10) + '0');
+			putchar((numba2 % 10) + '0');
+			if (numba1 == 9 && numba2 == 9)
+			{
+				continue;
+			}
+			else if (numba1 == 97 && numba2 == 98)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
