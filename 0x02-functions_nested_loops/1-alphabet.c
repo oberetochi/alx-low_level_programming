7df74fa843b439f548fc3080feda308be2635003
Nwosa <oberetochi@gmail.com>
2022-12-13 11:20:56 -0800

    Using custom header to print a file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,13 @@
  * main - entry point
  *
  * description: Using custom header to print alphabets777
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char alpha;
+
 	alpha = 'a';
 
 	while (alpha <= 'z')
@@ -16,5 +18,6 @@ void print_alphabet(void)
 		_putchar(alpha);
 		alpha++;
 	}
+	_putchar('\n');
 }
 
