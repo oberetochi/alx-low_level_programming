b2d2d0dfa0326d2d5f26fc54d8c6e6dc9a3d05d9
Nwosa <oberetochi@gmail.com>
2022-12-13 13:47:32 -0800

    Prints last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,8 +15,8 @@ int print_last_digit(int n)
 	last_digits = n % 10;
 	if (last_digits < 0)
 	
-		last_digits = last_digits * (-1);
-		_putchar(last_digits + '0');
+	last_digits = last_digits * (-1);
+	_putchar(last_digits + '0');
 	
 	return (last_digits); 
 }
