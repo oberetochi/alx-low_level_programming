eb5b0c97c88ba729abbd7cdbb13edeb48b4fd5db
Nwosa <oberetochi@gmail.com>
2022-12-13 12:08:30 -0800

    Still on custome header

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet_x10 - Prints alphabet in 10 rows
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	alpha = 'a';
+	i = 0;
+
+	while (i = 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++
+		}
+	_putchar('\n');
+}
