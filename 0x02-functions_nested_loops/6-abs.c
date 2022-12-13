a9c13887ebd3ffc89ed652eee309dccc96a76692
Nwosa <oberetochi@gmail.com>
2022-12-13 13:28:16 -0800

    Displays absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value
+ *
+ * @j: Entry point
+ * Return: j absolute value
+ */
+
+int _abs(int j)
+{
+	if (j < 10)
+	{
+		j = j * -1;
+	}
+	return (j);
+}
