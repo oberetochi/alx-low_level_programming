44dbc5a9344b77094def904f3af3c5bf15c2648a
Nwosa <oberetochi@gmail.com>
2022-12-13 12:31:22 -0800

    Picks out lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * int _islower - Picks out the lowercase character of the alphabets
+ * _islower - Picks out the lowercase character of the alphabets
  * Return: 1 if successful or 0 if unsuccessful
  */
 int _islower(int c)
