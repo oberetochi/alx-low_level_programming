4ab9b5afdfe06c8e0d825831638cec3ae77e5b4b
Nwosa <oberetochi@gmail.com>
2022-12-15 14:38:26 -0800

    Mutiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int mul - main block
+ *
+ * description: multiplies 2 numbers
+ * @a: first number entry point
+ * @b: second number entry point
+ * Return: Mutiplication of 2 numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
