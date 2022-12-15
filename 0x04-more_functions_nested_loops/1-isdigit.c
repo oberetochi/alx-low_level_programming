1bbae5b465ee0c69934dfe70dd242b268b92380c
Nwosa <oberetochi@gmail.com>
2022-12-15 14:21:31 -0800

    Checks for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - Main block
+ *
+ * description: chevks for digits between 0 through 9
+ *
+ * @c: entry point
+ *
+ * Returns: 1 if digit and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' &&  c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
