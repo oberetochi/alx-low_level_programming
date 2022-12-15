ba940afad38f4fe8109d65fc065ed800acf9e6d3
Nwosa <oberetochi@gmail.com>
2022-12-15 01:52:49 -0800

    Debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,19 +1,14 @@
-#include <stdlib.h>
-#include <stdio.h>
+
 #include "main.h"
-#include <time.h>
 /* more headers goes there */
 /**
- * positive_or_negative - Equates numbers
- *@i: entry point
- * description: equality matching
+ * positive_or_negative - Entry point
  *
+ * description: equality matching
  */
+
 void positive_or_negative(int i)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (i < 0)
 	{
 		printf("%d is negative\n", i);
