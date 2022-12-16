c4895ec4dfdf4feacfd983bb4a8e6ef0a7d5244f
Nwosa <oberetochi@gmail.com>
2022-12-15 22:55:22 -0800

    Prints integers with unsigned int

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - main block
+ *
+ * description: prints an integer
+ *
+ * @n: entry point
+ */
+
+void print_number(int n)
+{
+	unsigned int digit;
+
+	digit = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		digit = -digit;
+	}
+	if ((digit / 10) > 0)
+	{
+		print_number(digit / 10);
+	}
+	_putchar((digit % 10) + 48);
+}
