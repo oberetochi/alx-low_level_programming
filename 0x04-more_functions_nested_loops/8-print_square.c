0672288a99f2c84d36d7854f999f295d6e66137f
Nwosa <oberetochi@gmail.com>
2022-12-15 18:07:50 -0800

    Prints square of a number denotes by size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - main block
+ *
+ * description: prints square followes by a newline
+ * the square will be denoted by #
+ *
+ * @size: the square of the number
+ */
+
+void print_square(int size)
+{
+	int height, width;
+
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+			{
+				_putchar('#');
+			}
+		
+			if (height == size - 1)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+
+	}
+	_putchar('\n');
+}
