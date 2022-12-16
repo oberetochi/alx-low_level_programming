769f6a80025362df1b64a8bde0fa0285514c37e7
Nwosa <oberetochi@gmail.com>
2022-12-15 18:44:10 -0800

    Fizz Buzz FizzBizz puzzle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * description: A program that prints numbers 1 to 100
+ * it prints FIZZ for multiples of 3
+ * it peints BUZZ for multiples of 5
+ * it prints FIZZBUZZ for multiplea of 3 and 5
+ *
+ * Return: num if successful
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		if (num == 100)
+		{
+			continue;
+		}
+	}
+	printf("\n");
+}
