e76a5d7ef8f302e9e5fabb367853e1fce95dad8e
Nwosa <oberetochi@gmail.com>
2022-12-15 16:47:03 -0800

    Print a straight based on user input

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - main block
+ *
+ * description: prints a straight from the user input
+ *
+ * @n: entry point
+ */
+
+void print_line(int n)
+{
+	int length;
+
+	if (n > 0)
+	{
+		for (length = 0; length <= n; length++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
