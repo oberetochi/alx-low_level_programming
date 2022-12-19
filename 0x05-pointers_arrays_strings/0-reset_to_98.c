119ddb4daa845259ed0b27a0478b4771ed156c2e
Nwosa <oberetochi@gmail.com>
2022-12-19 13:34:31 -0800

    A function that takes a pointer to an int as parameter and updates the value it points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,7 @@
  * description: A function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
  *
- * @int *n: Entry point
+ * @n: Entry point
  *
  * Return: n
  */
