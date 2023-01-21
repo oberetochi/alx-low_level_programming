66a28cc9b7c3e7c2eefbbd5dd2e0120f0096aa60
Nwosa <oberetochi@gmail.com>
2023-01-21 10:21:32 -0800

    A  program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,6 +25,16 @@ return (1);
 
 /**
  * main - entry point
+ * 
+ * Description: program that adds positive numbers
+ * Print the result, followed by a new line
+ * If no number is passed to the program, print 0,
+ * followed by a new line
+ * If one of the number contains symbols that are not digits,
+ * print Error, followed by a new line, and return 1
+ * You can assume that numbers and the addition of
+ * all the numbers can be stored in an int
+ *
  * @argc: argument counter
  * @argv: argument vector
  * Return: return 0 if successful
