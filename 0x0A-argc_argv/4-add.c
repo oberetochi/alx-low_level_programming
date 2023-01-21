f3a7e5838fa6db546ed2da8c15a915006d243558
Nwosa <oberetochi@gmail.com>
2023-01-21 10:17:42 -0800

    A  program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @str: string to check
+ * Return: 1 if str contains only digits, return 0 if successful
+ */
+
+int is_digit(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (!isdigit(str[i]))
+{
+return (0);
+}
+}
+
+return (1);
+}
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: return 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+int sum = 0;
+
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+for (i = 1; i < argc; i++)
+{
+if (is_digit(argv[i]))
+{
+sum += atoi(argv[i]);
+/* atoi is used to convert a string to an integer */
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
